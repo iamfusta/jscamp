2795b2e71fcb1a6e9ee958f1960a3288c647ca9f
iamfusta <fusta64@gmail.com>
2021-10-19 00:15:58 +0300

    18.10.2021 Fusta (Engin Demiroğ ders sonu)

---
+++ src/app.js
@@ -0,0 +1,33 @@
+console.log("Merhaba kodlama.io")
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//pascalCasing naming commencal
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredsi","Özel Konut Kredsi" ]
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length; i++)
+{
+console.log("<li>"+konutKredileri[i]+"</li>")
+
+}
+console.log("<ul>")
+
+//let users = []
+
+console.log(konutKredileri)
